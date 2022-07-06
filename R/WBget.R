03b2f61ee78761416f8c34c497d229deb2156d03
spAdmin <spadmin@nadel-w17.d.ethz.ch>
2022-07-06 18:01:38 +0200

    automate selenium by data format

--- R/WBget.R
+++ R/WBget.R
@@ -1,3 +1,8 @@
+
+
+
+
+
 # WBget <- function(
 #         idno,
 #         query,
@@ -16,117 +21,171 @@ library(tidyverse)
 library(RSelenium)
 library(netstat)
 
-
-#### webscrape using Selenium
-
-# set url
-url = "https://microdata.worldbank.org/index.php/auth/login?destination=catalog/4444/get-microdata"
-
 # set user details
+item.id = 408
+dir <- "C:/Users/spadmin/Desktop/" # user-indicated dir
 user.email <- "m.weidinger@student.maastrichtuniversity.nl"
 user.pw <- "MiNh3mACz6yc9Tw"
-user.abstract <- "I am running a study of poverty and inequality across sub-saharan nations, using a novel method comparing horizontal inequalities in asset indeces"
-
-# webscrape data from microdata library
-
-# open server
-rs_driver_object <- RSelenium::rsDriver(
-    browser = "chrome",
-    chromever = "103.0.5060.53",
-    verbose = FALSE,
-    port = netstat::free_port()
-)
-
-# run client
-remDR <- rs_driver_object$client
-remDR$open()
-
-# navigate to url
-remDR$navigate(url)
-
-# sense email, password, and login fields
-email <- remDR$findElement(using = 'xpath', '//input[@id="email"]')
-passw <- remDR$findElement(using = 'xpath', '//input[@id="password"]')
-login <- remDR$findElement(using = 'xpath', '//input[@value="Login"]')
-
-# click and enter email
-email$clickElement()
-email$sendKeysToElement(list(user.email))
-
-# click and enter password
-passw$clickElement()
-passw$sendKeysToElement(list(user.pw))
-
-# click login
-login$clickElement()
-
-# # fill in abstract
-# abstract <- remDR$findElement(using = 'xpath', '//textarea[@id="abstract"]')
-# abstract$clickElement()
-# abstract$sendKeysToElement(list(user.abstract))
-#
-#
-# agree <- remDR$findElement(using = 'xpath', '//input[@id="chk_agree"]')
-# agree$clickElement()
-#
-# submit <- remDR$findElement(using = 'xpath', '//input[@id="submit"]')
-# submit$clickElement()
 
 
-download_button <- remDR$findElement(using = 'xpath', '//div[@class="resource-right-col float-right"]/a')
-download_button$getElementAttribute("href") -> dl_add
+#### webscrape using Selenium
+# dir defaults to current working directory
+# abstract defaults to Sect. 1.10.32 of Cicero's "de Finibus Bonorum et Malorum"
+
+WBdownload <- function(
+        item.id,
+        dir = getwd(),
+        unzip = TRUE,
+        user.email,
+        user.password,
+        user.abstract = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        ){
+    # set url
+    url = paste0("https://microdata.worldbank.org/", # root url
+                 "index.php/",  # go to index.php
+                 "auth/login?destination=catalog/", # authorize
+                 item.id, # paste dataset numeric ID
+                 "/get-microdata") # go to data repo
+
+    # webscrape data from microdata library
+
+    # open server
+    rs_driver_object <- RSelenium::rsDriver(
+        browser = "chrome",
+        chromever = "103.0.5060.53",
+        verbose = FALSE,
+        port = netstat::free_port()
+    )
+
+    # run client
+    remDR <- rs_driver_object$client
+    remDR$open()
+
+    # navigate to url
+    remDR$navigate(url)
+
+    # sense email, password, and login fields
+    email <- remDR$findElement(using = 'xpath', '//input[@id="email"]')
+    passw <- remDR$findElement(using = 'xpath', '//input[@id="password"]')
+    login <- remDR$findElement(using = 'xpath', '//input[@value="Login"]')
+
+    # click and enter email
+    email$clickElement()
+    email$sendKeysToElement(list(user.email))
+
+    # click and enter password
+    passw$clickElement()
+    passw$sendKeysToElement(list(user.pw))
+
+    # click login
+    login$clickElement()
+
+    # if there is an abstract required...
+    if(length(remDR$findElements(using = 'xpath', '//textarea[@id="abstract"]'))!=0){
+
+        # find web content: abstract, check box, and subitm button and body
+        abstract <- remDR$findElement(using = 'xpath', '//textarea[@id="abstract"]')
+        agree <- remDR$findElement(using = 'xpath', '//input[@id="chk_agree"]')
+        submit <- remDR$findElement(using = 'xpath', '//input[@id="submit"]')
+        body <- remDR$findElement("css", "body")
+
+        # fill in abstract
+        abstract$clickElement()
+        abstract$sendKeysToElement(list(user.abstract))
+        #scroll down
+        body$sendKeysToElement(list(key = "end"))
+        # sit back and wait
+        Sys.sleep(3)
+        # agree with terms and conditions
+        agree$clickElement()
+        # sit back and wait
+        Sys.sleep(3)
+        # submit request
+        submit$clickElement()
+    }
+
+    # sit back and wait
+    Sys.sleep(10)
+
+    ###THIS WORKS, BUT USES FIRST ZIP WHICH ISNT NECESSARILY RIGHT...WANT TO SELECT BY PARTIAL STRING MATCH "CSV.zip", BUT NOT SURE HOW
+    # download_button <- remDR$findElement(using = 'xpath',
+    #                     '//a[@data-extension="zip"]')
+
+    download_button <- remDR$findElement(using = 'xpath',
+                                         '//a[@data-filename="_CSV.zip"]')
+
+    download_button$getElementAttribute("href") -> dl_add
+
+    #### SAVE THE DOWNLOADS (USING ONE OF TWO OPTIONS)
+
+    ### OPTION 1 - save in temporary directory:
+
+    # # create temporary directory
+    # temp <- tempdir()
+    #
+    # # download zip file to temporary directory
+    # download.file(url = dl_add[[1]], destfile = paste0(temp,"/",4444, "wbget.zip"), mode = "wb")
+    #
+    # # see content of temporary directory
+    # list.files(temp) # one zip file
+    #
+    # # unzip downloads
+    # unzip(paste0(temp,"/",4444, "wbget.zip"), exdir = paste0(temp,"/",4444, "wbget"))
+    #
+    # # inspect unzipped files
+    # list.files(paste0(temp,"/",4444, "wbget")) # one zip file
+    #
+    # # INSERT USE OF DATA HERE...
+    #
+    # # delete temporary directory
+    # unlink(temp)
 
 
+    ### OPTION 2 - save in permanent, user-indicated directory
 
-#### SAVE THE DOWNLOADS (USING ONE OF TWO OPTIONS)
+    # paste together a directory for the download
+    dl_dir <- paste0(dir,"wbget_",format(Sys.time(), "%d_%b_%Y_%H_%M"))
 
-### OPTION 1 - save in temporary directory:
+    # create download directory
+    dir.create(file.path(dl_dir))
 
-# # create temporary directory
-# temp <- tempdir()
-#
-# # download zip file to temporary directory
-# download.file(url = dl_add[[1]], destfile = paste0(temp,"/",4444, "wbget.zip"), mode = "wb")
-#
-# # see content of temporary directory
-# list.files(temp) # one zip file
-#
-# # unzip downloads
-# unzip(paste0(temp,"/",4444, "wbget.zip"), exdir = paste0(temp,"/",4444, "wbget"))
-#
-# # inspect unzipped files
-# list.files(paste0(temp,"/",4444, "wbget")) # one zip file
-#
-# # INSERT USE OF DATA HERE...
-#
-# # delete temporary directory
-# unlink(temp)
+    # paste together zip-file name
+    dl_file <- paste0(dl_dir,"/wb",item.id, ".zip")
 
+    # download zipped data folder
+    download.file(url = dl_add[[1]], destfile = dl_file, mode = "wb")
 
-### OPTION 2 - save in permanent, user-indicated directory
+    if (unzip == FALSE){
+        message(paste0("SUCCESS: ",item.id," has been downloaded to ",dl_dir,"."))
+    } else {
+        message(paste0("unzipping files for item id ",item.id,"..."))
+        unz_dir <- paste0(dl_dir,"/","wb",item.id)
+        dir.create(file.path(unz_dir))
+        # unzip data files into download directory
+        unzip(dl_file, exdir = unz_dir)
+        file.remove(dl_file)
+        message(paste0("SUCCESS: ",item.id," has been downloaded and unzipped to ", dl_dir,"."))
+    }
 
-dir <- "C:/Users/spadmin/Desktop/" # user-indicated dir
+    ### close server
 
-# paste together a directory for the download
-dl_dir <- paste0(dir,"wbget_",format(Sys.time(), "%d_%b_%Y_%H_%M"))
+    remDR$closeWindow()
 
-# create download directory
-dir.create(file.path(dl_dir))
+    rs_driver_object <- rs_driver_object$server$stop()
 
-# paste together zip-file name
-dl_file <- paste0(dl_dir,"/",4444, "wbget.zip")
+    remDR$closeall()
 
-# download zipped data folder
-download.file(url = dl_add[[1]], destfile = dl_file, mode = "wb")
+    # kill java and Chromedriver to finish task
+    message("terminating background processes...")
 
-# unzip data files into download directory
-unzip(dl_file, exdir = dl_dir)
+    system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
+    system("taskkill /F /IM ChromeDriver.exe", intern=FALSE, ignore.stdout=FALSE)
+    message("End of WBdownload cycle")
+
+}
 
 
-
-### KILL JAVA
-
-system('taskkill /im java.exe /f')
+WBdownload(item.id = item.id, user.email = user.email, user.password = user.pw, dir = dir, unzip = TRUE)
 
 # dl_add now contains the link to the zipped folder on the microdata library that contains the files of interest.
 #
@@ -169,6 +228,6 @@ for (i in 2:length(dta)){
     df %<>% merge(dta[[i]])
 }
 
-dta <- tibble(idno = idno, alpha3 = substr(idno,1,3))
+dta <- tibble(idno = idno, iso3 = substr(idno,1,3))
 
 dta %<>% distinct()
