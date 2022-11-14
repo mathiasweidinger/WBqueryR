1bb0cd8de349d5e9ae72bbcd808c10af1f81f833
mathiasweidinger <mathias.weidinger@gmail.com>
2022-11-14 15:59:13 +0100

    update chromedriver

--- R/WBdownload.R
+++ R/WBdownload.R
@@ -34,6 +34,7 @@ WBdownload <- function(
         user.password,
         user.abstract = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 ){
+
     # set url
     url = paste0("https://microdata.worldbank.org/", # root url
                  "index.php/",  # go to index.php
@@ -46,7 +47,7 @@ WBdownload <- function(
     # open server
     rs_driver_object <- RSelenium::rsDriver(
         browser = "chrome",
-        chromever = "103.0.5060.53",
+        chromever = "107.0.5304.62",
         verbose = FALSE,
         port = netstat::free_port()
     )
